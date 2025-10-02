e414b322c1618e009e6eb7decf17b09b8b20a7aa
RoyDMAP <roy.dimapilis@yahoo.com>
2025-10-01 17:27:54 -0700

    Initial Commit

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Roy Dimapilis on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
